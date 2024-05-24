80d1f0e4a234f67d391ba0e9f167c9698dd71bee
Jose Gonzalez <coding.tamalito@gmail.com>
2024-05-24 16:58:23 +0200

    add different types of expenses

---
+++ utils/types.js
@@ -0,0 +1,16 @@
+// will contain all the types of expenses
+const expensesTypes = {
+    ESSENTIAL_FOOD: "essential_food",
+    NON_ESSENTIAL_FOOD: "non_essential_food",
+    PARTY: "party",
+    PHONE: 'phone',
+    INSURANCE: 'insurance',
+    CASH: 'cash',
+    HOME: 'home',
+    GIFT: 'gift',
+    RECREATIONAL_PURCHASE: 'recreational_purchase',
+    RENT: 'rent',
+    OTHER: 'other'
+}
+
+module.exports = expensesTypes;
