a27a4639d59001e4c8c83a54639b84160148814d
Jose Gonzalez <coding.tamalito@gmail.com>
2024-06-04 20:44:38 +0200

    add route to get expenses for a month

--- routes/expenses.routes.js
+++ routes/expenses.routes.js
@@ -9,4 +9,6 @@ router.post('/addExpense', expensesController.addExpense);
 
 router.get('/getExpenseForMonth/:month/:year', expensesController.getExpensesForAMonth);
 
+router.get('/getExpensesOfType/:month/:year', expensesController.getExpensesOfATypeForAMonth);
+
 module.exports = router;
