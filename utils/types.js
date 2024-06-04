cfd4f9254a41189a1ea890803f58a9341c1f32af
Jose Gonzalez <coding.tamalito@gmail.com>
2024-06-04 20:45:50 +0200

    add the different types of expenses

--- utils/types.js
+++ utils/types.js
@@ -1,7 +1,7 @@
 // will contain all the types of expenses
 const expensesTypes = {
     ESSENTIAL_FOOD: "essential_food",
-    NON_ESSENTIAL_FOOD: "non_essential_food",
+        NON_ESSENTIAL_FOOD: "non_essential_food",
     PARTY: "party",
     PHONE: 'phone',
     INSURANCE: 'insurance',
