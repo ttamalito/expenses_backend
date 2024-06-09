0b344a221a4a996eb6ba1626544074929a0cd5b0
Jose Gonzalez <coding.tamalito@gmail.com>
2024-06-09 20:18:37 +0200

    modify routes

--- routes/expenses.routes.js
+++ routes/expenses.routes.js
@@ -9,6 +9,6 @@ router.post('/addExpense', expensesController.addExpense);
 
 router.get('/getExpenseForMonth/:month/:year', expensesController.getExpensesForAMonth);
 
-router.get('/getExpensesOfType/:month/:year', expensesController.getExpensesOfATypeForAMonth);
+router.post('/getExpensesOfType/:month/:year', expensesController.getExpensesOfATypeForAMonth);
 
 module.exports = router;
