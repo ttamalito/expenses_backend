6010aa5f6972ed4142f5c19487980b241ce8a164
Jose Gonzalez <coding.tamalito@gmail.com>
2024-07-31 21:34:53 +0200

    add comment

--- routes/setup.routes.js
+++ routes/setup.routes.js
@@ -7,6 +7,7 @@ const setUpController = require('../controllers/setup.controller');
 
 router.get('/getSetUp/:year', setUpController.getSetUp);
 
+// route to create modify the existsing setup
 router.post('/modifySetUp/:year', setUpController.modifySetUp);
 
 
