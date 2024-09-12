d951501f640bb4e2a6fe8aa0a2d44b7796eaf8a8
Jose Gonzalez <coding.tamalito@gmail.com>
2024-09-12 11:55:05 +0200

    Add income route

---
+++ routes/income.routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const router = express.Router();
+
+const incomeController = require('../controllers/income.controller');
+
+router.get('/total-earned', incomeController.getTotalEarnedForAYear); // query params: year
+
+module.exports = router;
