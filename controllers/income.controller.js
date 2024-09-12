ae4ec20834755f4ef584b3d50e56a30910f3f017
Jose Gonzalez <coding.tamalito@gmail.com>
2024-09-12 11:54:45 +0200

    Add icome controller

---
+++ controllers/income.controller.js
@@ -0,0 +1,27 @@
+const setUpModel = require('../models/setup.model');
+const expensesTypes = require('../utils/types');
+const incomeModel = require('../models/income.model');
+
+/**
+ * Retrieves the total earnings for a given year
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise<*>}
+ */
+async function getTotalEarnedForAYear(req, res, next) {
+    const year = parseInt(req.query.year);
+
+    try {
+        const total = await incomeModel.queryTotalEarnedForAYear(year);
+        return res.status(200).json({total: total});
+    } catch (error) {
+        return res.status(500).json({error: error, message: 'Failed to get total earnings' });
+    }
+
+}
+
+
+module.exports = {
+    getTotalEarnedForAYear
+}
