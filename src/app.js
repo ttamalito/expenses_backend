cdae9fb999ee34f10575ea66349579cefc452a75
Jose Gonzalez <coding.tamalito@gmail.com>
2024-09-16 09:43:37 +0200

    Refactor app to have a server.js file

--- src/app.js
+++ src/app.js
@@ -3,15 +3,13 @@ const express = require('express');
 
 //const path = require('path');
 // import files
-const db = require('./database/databaseConfig');
 const addCORSHeader = require('./middlewares/addCORSHeader');
 
 
 //const bodyParser = require('body-parser');
 
 
-const PORT = 'mongodb://localhost:27017';
-const databaseName = 'expenses_db';
+
 // import the routes
 const baseRoutes = require('./routes/base.routes');
 const expenseRoutes = require('./routes/expenses.routes')
@@ -39,17 +37,6 @@ app.use(expenseRoutes);
 app.use(setUpRoutes);
 app.use( '/income',incomeRoutes);
 // the http server
-let server;
-// start listening, if we connect to the database
-db.connectToDataBase(PORT, databaseName).then(
-    () => {
-        // the promise was fulfilled
-        server = app.listen(8080);
-        console.log('Listening on port 8080');
-    }
-).catch(
-    (error) => {
-        console.error(error);
-        console.log('No connection to the database established')
-    }
-)
+
+
+module.exports = app;
