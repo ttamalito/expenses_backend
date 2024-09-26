0f5b4d9b5b394efafe0e3cf9bb65142a29d3eb76
Jose Gonzalez <coding.tamalito@gmail.com>
2024-09-26 08:59:46 +0200

    Add tests for set up

---
+++ test/controllers/setup.controller.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest')
+
+const app = require('../../src/app');
+const PORT = 'mongodb://localhost:27017';
+const databaseName = 'expenses_db';
+const db = require("../../src/database/databaseConfig");
+
+describe('Query set up for a year', () => {
+
+    beforeAll(async () => {
+        await db.connectToDataBase(PORT, databaseName);
+    });
+
+    afterAll(async () => {
+        await db.closeConnection();
+    });
+    it('should query successfully set up', async () => {
+        const year = '2024';
+        const endpoint = `/getSetUp/${year}`;
+        const response = await request(app).get(endpoint);
+        expect(response.status).toEqual(200);
+    });
+
+
+})
