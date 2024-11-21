5c0730f537997cbbd936f5705e98e5dbdc1b9014
Jose Gonzalez <coding.tamalito@gmail.com>
2024-11-21 08:00:42 +0100

    Add endpoint to fetch total earned in a monthly basis

--- src/controllers/income.controller.js
+++ src/controllers/income.controller.js
@@ -22,6 +22,41 @@ async function getTotalEarnedForAYear(req, res, next) {
 }
 
 
+/**
+ * Retrieves the total earnings for a given month
+ * @param req
+ * @param res
+ * @param next
+ * @returns {Promise<*>}
+ */
+async function getTotalEarnedInAMonth(req, res, next) {
+    const year = parseInt(req.query.year);
+    const month = parseInt(req.query.month);
+
+    try {
+        const total = await incomeModel.queryTotalEarnedForAMonth(month, year);
+        return res.status(200).json({total: total});
+    } catch (error) {
+        return res.status(500).json({error: error, message: 'Failed to get total earnings' });
+    }
+
+}
+
+async function getTotalEarnedInAYearInAMonthlyBasis(req, res, next) {
+    const year = parseInt(req.query.year);
+
+    try {
+        const result = await incomeModel.queryTotalEarnedForAYearInAMonthlyBasis(year);
+        return res.status(200).json(result);
+    } catch (error) {
+        return res.status(500).json({error: error, message: 'Failed to get total earnings' });
+    }
+}
+
+
+
 module.exports = {
-    getTotalEarnedForAYear
+    getTotalEarnedForAYear,
+    getTotalEarnedInAMonth,
+    getTotalEarnedInAYearInAMonthlyBasis
 }
